fc800a17b1d47a35cf91460af2b0b92b0721f316
zzhhdaaa <zzhhdaaa@g.ucla.edu>
2021-10-25 00:37:56 -0700

    Demo03

---
+++ Projects/mTm_DEMO_03/Assets/Scripts/MoveToTarget.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToTarget : MonoBehaviour
+{
+    public bool feiLe = false;
+    public GameObject target;
+    public bool tingLe = false;
+    public bool daoLe = false;
+    public float tingXia = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ZhongShenGuiWei();
+    }
+
+    void ZhongShenGuiWei()
+    {
+        if (feiLe && !daoLe)
+        {
+            if (gameObject.GetComponent<Rigidbody>() != null)
+            {
+                if (gameObject.GetComponent<Rigidbody>().velocity.magnitude < tingXia)
+                {
+                    tingLe = true;
+                    Destroy(gameObject.GetComponent<Rigidbody>());
+                }
+            }
+            else
+            {
+                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, target.transform.position, Time.deltaTime);
+                gameObject.transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, target.transform.rotation, Time.deltaTime);
+                if (Vector3.Distance(gameObject.transform.position, target.transform.position) < 1.0f)
+                {
+                    target.GetComponent<MeshRenderer>().enabled = false;
+                    gameObject.transform.position = target.transform.position;
+                    gameObject.transform.rotation = target.transform.rotation;
+                    //daoLe = true;
+                }
+            }
+        }
+        else
+        {
+            gameObject.AddComponent<Rigidbody>();
+        }
+    }
+}
---
+++ Projects/mTm_DEMO_03/Assets/Scripts/ZhongShenGuiWei.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZhongShenGuiWei : MonoBehaviour
+{
+    public Dictionary<GameObject, GameObject> GuiWei = new Dictionary<GameObject, GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
