b18cbcf9fe88e078dbf1282640f9e2dabaa49343
yinong16 <ge98@ucla.edu>
2021-10-25 02:06:43 -0700

    Demo03

--- Projects/mTm_DEMO_03/Assets/Scripts/ZhongShenGuiWei.cs
+++ Projects/mTm_DEMO_03/Assets/Scripts/ZhongShenGuiWei.cs
@@ -20,6 +20,36 @@ public class ZhongShenGuiWei : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GaiWuDing();
+    }
+    void GaiWuDing()
+    {
+        if (!quanWanShir)
+        {
+            int wanShirShuLiang = 0;
+            for (int i = 0; i < cubes.Length; i++)
+            {
+                wanShirLe[i] = cubes[i].GetComponent<MoveToTarget>().wanShirLe;
+                if (wanShirLe[i])
+                {
+                    wanShirShuLiang += 1;
+                }
+            }
+            if (wanShirShuLiang >= cubes.Length)
+            {
+                for (int i = 0; i < cubes.Length; i++)
+                {
+                    cubes[i].AddComponent<Rigidbody>();
 
+                }
+                roof.SetActive(true);
+                Debug.Log("GaiTMD!!");
+                quanWanShir = true;
+            }
+        }
+        else
+        {
+            return;
+        }
     }
 }
