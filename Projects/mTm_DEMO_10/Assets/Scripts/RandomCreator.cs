c39d2b81379c8f59e70ebe8dde2c36ecb6b0bdf6
yinong16 <ge98@ucla.edu>
2021-12-03 05:04:05 -0800

    10
    10

---
+++ Projects/mTm_DEMO_10/Assets/Scripts/RandomCreator.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCreator : MonoBehaviour
+{
+    public GameObject target;
+    public float offset;
+    public float gap;
+    private Vector3 initialTarget;
+    private GameObject clickBrick;
+    private Rigidbody rb;
+    public float clickForce = 0.5f;
+
+    [HideInInspector]
+    public int clickTimes = -1;
+
+    void Start()
+    {
+        MouseManager.Instance.OnBrickClicked += ClickBrick;//订阅鼠标点击事件
+        initialTarget = target.transform.position;
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void ClickBrick(GameObject brick)
+    {
+        if (brick != null)
+        {
+            clickTimes += 1;
+            clickBrick = brick;
+            rb = clickBrick.GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, clickForce);
+            if (clickBrick.GetComponent<MoveToRandomTarget>() != null)
+            {
+                if (!clickBrick.GetComponent<MoveToRandomTarget>().settled)
+                {
+                    target.transform.position = new Vector3(initialTarget.x + Random.Range(-offset, offset), gap * clickTimes, initialTarget.z + Random.Range(-offset, offset));
+                    target.transform.Rotate(new Vector3(0f, Random.Range(0f, 360f), 0f));
+                    clickBrick.GetComponent<MoveToRandomTarget>().targetPos = target.transform.position;
+                    clickBrick.GetComponent<MoveToRandomTarget>().targetRot = target.transform.rotation;
+                    clickBrick.GetComponent<MoveToRandomTarget>().flyingOut = true;
+                }
+                else
+                {
+                    clickBrick.GetComponent<MoveToRandomTarget>().targetPos = clickBrick.GetComponent<MoveToRandomTarget>().startPos;
+                    clickBrick.GetComponent<MoveToRandomTarget>().targetRot = clickBrick.GetComponent<MoveToRandomTarget>().startRot;
+                    clickBrick.GetComponent<MoveToRandomTarget>().settled = false;
+                    clickBrick.GetComponent<MoveToRandomTarget>().flyingOut = true;
+                }
+            }
+        }
+    }
+}
