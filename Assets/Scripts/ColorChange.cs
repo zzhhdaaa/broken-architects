9672c2a054504e4238ada4b3185b9ede05248720
zzhhdaaa <zzhhdaaa@g.ucla.edu>
2022-04-29 01:52:17 -0700

    after-class-update
    Update:
    +WebGL version.
    +Controller/Keyboard Mouse mode
    +New UI version
    +Bug fix
    +High-res UI scaler

---
+++ Assets/Scripts/ColorChange.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    private Material[] startMaterials;
+    public Material hoverMaterial;
+    private Material[] hoverMaterials;
+    private CursorManager cursorManager;
+    private RaycastHit hitInfo;
+
+    private void Start()
+    {
+        if (GetComponent<Renderer>() != null)
+        {
+            startMaterials = GetComponent<Renderer>().materials;
+            hoverMaterials = new Material[startMaterials.Length + 1];
+            for (int i = 0; i < hoverMaterials.Length; i++)
+            {
+                hoverMaterials[i] = hoverMaterial;
+            }
+        }
+        cursorManager = FindObjectOfType<CursorManager>();
+    }
+
+    private void Update()
+    {
+        ChangeTheColor();
+    }
+
+    /*private void OnMouseEnter()
+    {
+        GetComponent<Renderer>().materials = hoverMaterials;
+    }
+
+
+    private void OnMouseExit()
+
+    {
+        GetComponent<Renderer>().materials = startMaterials;
+    }*/
+
+    private void ChangeTheColor()
+    {
+        if (cursorManager != null)
+        {
+            Ray rayCursor = Camera.main.ScreenPointToRay(cursorManager.rect.position);
+            if (Physics.Raycast(rayCursor, out hitInfo))
+            {
+            if (hitInfo.collider == this.GetComponent<BoxCollider>() || hitInfo.collider == this.GetComponent<MeshCollider>())
+            {
+                GetComponent<Renderer>().materials = hoverMaterials;
+            }
+            else
+            {
+                GetComponent<Renderer>().materials = startMaterials;
+            }
+            }
+        }
+
+        Ray rayMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(rayMouse, out hitInfo))
+        {
+            if (hitInfo.collider == this.GetComponent<BoxCollider>() || hitInfo.collider == this.GetComponent<MeshCollider>())
+            {
+                GetComponent<Renderer>().materials = hoverMaterials;
+            }
+            else
+            {
+                GetComponent<Renderer>().materials = startMaterials;
+            }
+        }
+    }
+}
